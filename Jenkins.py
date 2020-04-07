872902ad03e7ae15fc5e3d07d98de467748f844d
sagarfrancis <sagar.tekula@gmail.com>
2020-04-07 18:58:37 +0530

    working on jenkins via gitgubJenkins.py

---
+++ Jenkins.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import csv
+data=pd.read_csv("/home/sagar/Downloads/profile.csv.csv",low_memory=False)
+print(data.head(5))
+
+
